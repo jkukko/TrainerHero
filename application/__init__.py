8c4f32cf800105ca275a8257c7aa68478a7a02a5
jkukko <joonas.kukkonen@gmail.com>
2020-03-26 21:46:23 +0200

    kirjatuminen ja uloskirjautuminen lisätty

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+# Tietokanta
 from flask_sqlalchemy import SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///trainerHero.db"
 app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
+# Sovellukseksen toiminnallisuudet
 from application import views
 
 from application.movements import models
@@ -18,4 +20,21 @@ from application.muscle_group import views
 from application.auth import models
 from application.auth import views
 
+# kirjautuminen
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 db.create_all()
+
