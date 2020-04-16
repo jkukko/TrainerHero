bd2ee138d45a773ce63bf83d5b8a0543ad54d387
jkukko <joonas.kukkonen@gmail.com>
2020-04-16 15:12:17 +0300

    base model kommentoitu

--- application/models.py
+++ application/models.py
@@ -3,7 +3,7 @@ from application import db
 class Base(db.Model):
 
     __abstract__ = True
-  
+    #kommentti
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
