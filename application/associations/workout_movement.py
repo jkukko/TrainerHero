612dac7f9ad0a705d32ffe934c81c3db8ea199c9
jkukko <joonas.kukkonen@gmail.com>
2020-04-16 21:15:06 +0300

    musclegroup login required added

--- application/associations/workout_movement.py
+++ application/associations/workout_movement.py
@@ -1,4 +1,4 @@
-from application import db
+#from application import db
 
 #workout_movement = db.Table(
 #    'workout_movement', 
--- application/muscleGroup/views.py
+++ application/muscleGroup/views.py
@@ -1,17 +1,21 @@
 from application import app, db
 from flask import render_template, request, url_for, redirect
+from flask_login import login_required
 from application.muscleGroup.models import muscleGroup
 
 @app.route("/musclegroups", methods=["GET"])
+@login_required
 def musclegroup_index():
     return render_template("musclegroups/list.html", musclegroups = muscleGroup.query.all())
 
 @app.route("/musclegroups/new/", methods=["GET"])
+@login_required
 def musclegroup_form():
     return render_template("musclegroups/new.html", musclegroups = muscleGroup.query.all())
 
 
 @app.route("/musclegroups/", methods=["POST"])
+@login_required
 def musclegourp_create():
     name = request.form.get("name")
     m = muscleGroup(name)
